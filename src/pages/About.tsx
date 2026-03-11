02d7477e576c8a5e6a2cd546f46886684f80e36c
piotr-skolorzynski <p.skolorzynski@gmail.com>
2026-03-11 19:52:27 +0100

    Create About page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,8 +1,26 @@
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-    </div>
+    <>
+      <div className="flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center">
+        <h1 className="text-4xl font-bold leading-none tracking-tight sm:text-6xl">
+          We love
+        </h1>
+        <div className="stats bg-primary shadow">
+          <div className="stat">
+            <div className="stat-title text-primary-content text-4xl font-bold tracking-widest">
+              comfy
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className="mt-6 text-lg leading-8 max-w-2xl mx-auto">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum a cumque
+        ea. Quam illo aliquam reiciendis laborum voluptate ullam expedita
+        doloremque magni veritatis est. Consequatur qui officia, hic veritatis
+        quam voluptatum voluptatibus, nam unde ipsam quos, officiis ratione quia
+        corporis.
+      </p>
+    </>
   );
 };
 
